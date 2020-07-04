d36a74e8f716e5e32bc9d5ca82f85430bd5246da
Munish <munishmahey2@gmail.com>
2020-07-04 15:35:52 -0400

    finished signin handleSubmit method

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,7 +20,7 @@ const Header = ({ currentUser }) => (
 
             {
                 currentUser ? 
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                <div className="option" onClick={() => auth.signOut()}>LOGOUT</div>
                 :
                 <Link  className="option" to='/signIn'>
                     SIGN IN 
